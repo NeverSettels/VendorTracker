c55f1e19427b5c5b1988920bdead2cf73aa72d40
Eric Settels <settels.eric@gmail.com>
2020-05-22 08:46:39 -0700

    Work on basic model set up

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -4,6 +4,17 @@ namespace VendorTracker.Models
 {
   public class Order
   {
+    public string Name {get;set;}
+    public string Description {get;set;}
+    public int Price {get;set}
+    public string DeliveryDate {get;set}
+     public int Id  {get;}
+   private static List<Order>  _instances = new List<Order> {};
+    private static int _currentId = 0;
 
+
+    public Order()
+    {
+    }
   }
 }
--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -4,6 +4,10 @@ namespace VendorTracker.Models
 {
   public class Vendor
   {
-
+    public string Name {get;set;}
+    public string Description {get;set;}
+    public int Id  {get;}
+    private static int _currentId = 0;
+    private static List<Vendor> _instances = new List<Vendor> {};
   }
 }
