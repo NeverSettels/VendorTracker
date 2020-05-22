c096acf6f78da13d5f98e488328ba521cc94bd62
Eric Settels <settels.eric@gmail.com>
2020-05-22 09:36:55 -0700

    Add to vendor model  and home controller

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -19,5 +19,21 @@ namespace VendorTracker.Models
       _currentId++;
       _instances.Add(this);
     }
+  
+    public static List<Vendor> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+
   }
 }
